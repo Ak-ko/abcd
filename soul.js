4b714e2429b41c1edda4ec296b6d3a5df4405409
Aung Khant Ko Ko <waterbubbles.ww6615@gmail.com>
2022-07-07 00:27:15 +0630

    updating

--- soul.js
+++ soul.js
@@ -1,6 +1,8 @@
 const city = document.getElementById('city');
 const year = document.getElementById('year');
 const navbar = document.querySelector('.navbars');
+const degree_section = document.querySelector('.degree_sections');
+const navbar_container = document.querySelector('.navbar_container');
 
 city.textContent = 'Myanmar';
 year.textContent = new Date().getUTCFullYear();
@@ -8,15 +10,9 @@ year.textContent = new Date().getUTCFullYear();
 
 window.addEventListener('scroll', () => {   
     if(window.scrollY > navbar.getBoundingClientRect().height){
-        navbar.classList.add('navbars_top');
+        navbar.classList.add('navbars_top');        
     }else {
         navbar.classList.remove('navbars_top');
     }
-})
+});
 
-
-// For Subscribe button
-const accordion_button = document.querySelector('.accordionbuttons');
-accordion_button.addEventListener('click', () => {
-    accordion_button.classList.toggle('transparentBackground');
-})
